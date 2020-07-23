2c37717293d5571c3b95bda7956dfe33179c5062
Jeremy Huot <logflume@live.com>
2020-07-23 00:38:52 +0000

    Add queries to pull data from db in app

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
+FROM assistance_requests
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach((teacher) => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+  });
+})
+.catch(err => console.error('query error', err.stack));
