3fc7937a8a60f5e9f719585e86e5832306eb3349
Jeremy Huot <logflume@live.com>
2020-07-23 01:55:46 +0000

    Fix query fields to use parameter variables to protect against SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -10,13 +10,11 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+pool.query("SELECT students.id, students.name as name, cohorts.name as cohort \
+FROM students \
+JOIN cohorts ON cohorts.id = cohort_id \
+WHERE cohorts.name LIKE $1::VARCHAR(255) \
+LIMIT $2\;", [process.argv[2], process.argv[3] || 5])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,13 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
-FROM assistance_requests
-JOIN teachers ON teachers.id = assistance_requests.teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER BY teachers.name;
-`)
+pool.query('SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort \
+FROM assistance_requests \
+JOIN teachers ON teachers.id = assistance_requests.teacher_id \
+JOIN students ON students.id = student_id \
+JOIN cohorts ON cohorts.id = cohort_id \
+WHERE cohorts.name = $1 \
+ORDER BY teachers.name;',[process.argv[2]])
 .then(res => {
   res.rows.forEach((teacher) => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
